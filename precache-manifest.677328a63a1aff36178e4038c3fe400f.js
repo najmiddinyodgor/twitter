e0d8bc7dc973ae4ae14dda7474ccf4326454b4c5
Yodgor <najmiddinkhaled@gmail.com>
2020-06-27 19:42:03 +0500

    Updates

---
+++ precache-manifest.677328a63a1aff36178e4038c3fe400f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05dfcdd147e534117929b8eb001b14cb",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "c2024fad0bae0320e339",
+    "url": "/twitter/static/css/main.002be5b3.chunk.css"
+  },
+  {
+    "revision": "2374bed317535422f26f",
+    "url": "/twitter/static/js/2.e563f348.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/twitter/static/js/2.e563f348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2024fad0bae0320e339",
+    "url": "/twitter/static/js/main.00520a70.chunk.js"
+  },
+  {
+    "revision": "45d02702cb7383fced6b",
+    "url": "/twitter/static/js/runtime-main.3ec93ec1.js"
+  }
+]);
